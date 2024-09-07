fef3151cfd7638432dfd41f33abcc6e62497a63c
Gaurav Bisht <gaurav.bisht@tothenew.com>
2024-09-07 23:51:18 +0530

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Gaurav Bisht on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
